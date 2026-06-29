b97113931ad1046dd698afd64fe26168f75a4464
Umapathydsa <22f1001623@ds.study.iitm.ac.in>
2026-06-29 10:10:13 +0530

    fourth_commit

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ from typing import List
 from fastapi import FastAPI, Query, Request, Response
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    ALLOWED_ORIGIN = "https://dash-xfs84l.example.com",
+    MY_EMAIL = "22f1001623@ds.study.iitm.ac.in",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 ALLOWED_ORIGIN = "https://dash-xfs84l.example.com"
 MY_EMAIL = "22f1001623@ds.study.iitm.ac.in"
 
